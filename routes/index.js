05356d0a224080bce40381ef8325c8350a6bf391
yaburi <nathan.au@hotmail.com>
2018-07-12 16:53:35 +1000

    ready to branch

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,4 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'MusicList Alpha' });
 });
 
-const testJSON = [
-    {
-        name: 'Guy Fieri',
-        username: 'flavatown'
-    },
-    {
-        name: 'Joe Rogan',
-        username: 'onnit'
-    }
-];
-
-router.get('/sendjson', (req, res, next) => {
-    res.json(testJSON);
-});
-
 module.exports = router;
