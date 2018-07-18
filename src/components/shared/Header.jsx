b7862ef259c3d5ad4ca57e42318472d5d4985a45
yaburi <nathan.au@hotmail.com>
2018-07-18 17:37:30 +1000

    added login page with bootstrap

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,6 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
 
+export default class Header extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <header className="wrapper">
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggleNavbar} />
+          <NavbarBrand tag={Link} to="/">MusicList</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/account/login">Login</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </header>
+    );
+  }
+}
+
+/*
 export default function Header(props) {
   const { username } = props;
   return (
@@ -18,3 +55,4 @@ export default function Header(props) {
     </header>
   );
 }
+*/
